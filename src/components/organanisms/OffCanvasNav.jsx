110d0ee9ec6251f0da9e195674231ae2b34a3c5f
“JEREMY <Jeremyinoa67@gmail.com>
2024-03-21 21:17:54 -0400

    hotfix: routing for offcanvas ( chat )

--- src/components/organanisms/OffCanvasNav.jsx
+++ src/components/organanisms/OffCanvasNav.jsx
@@ -39,7 +39,7 @@ const OffCanvasNav = () => {
                 </li>
 
                 <li>
-                    <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-900 dark:hover:bg-gray-700 group">
+                    <a href="/chat" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-900 dark:hover:bg-gray-700 group">
                     <svg className="w-[25px] h-[25px] text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path fillRule="evenodd" d="M3 6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-6.616l-2.88 2.592C8.537 20.461 7 19.776 7 18.477V17H5a2 2 0 0 1-2-2V6Zm4 2a1 1 0 0 0 0 2h5a1 1 0 1 0 0-2H7Zm8 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2Zm-8 3a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H7Zm5 0a1 1 0 1 0 0 2h5a1 1 0 1 0 0-2h-5Z" clipRule="evenodd"/>
                     </svg>
