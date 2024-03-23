b44c780146a592c2551e91184a44dd33f9f60e26
“JEREMY <Jeremyinoa67@gmail.com>
2024-03-23 17:17:48 -0400

    feat: adds firebase sdk

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCldYTzfibXDeb3w_oAANduDaRH9ML5izA",
+    authDomain: "financit.firebaseapp.com",
+    projectId: "financit",
+    storageBucket: "financit.appspot.com",
+    messagingSenderId: "1046705062384",
+    appId: "1:1046705062384:web:581376634b3662c8745d35"
+};
+
+export default firebaseConfig;
